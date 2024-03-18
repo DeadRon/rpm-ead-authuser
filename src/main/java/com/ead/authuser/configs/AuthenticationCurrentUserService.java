c0c4fa31fb4d41d54d9237b8f64a592436860a0a
Ronaldo Melo <ronaldorpm1994@gmail.com>
2024-03-18 01:59:47 -0300

    [SEMANA 7][Cross-Cutting Authentication e Authorization com JWT em Microservices][Authorization com Roles em Microservices]

---
+++ src/main/java/com/ead/authuser/configs/AuthenticationCurrentUserService.java
@@ -0,0 +1,20 @@
+package com.ead.authuser.configs;
+
+import com.ead.authuser.configs.security.UserDetailsImpl;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+//Classe para extrair informações do usuário corrente na api
+public class AuthenticationCurrentUserService {
+
+    public UserDetailsImpl getCuurent(){
+        return (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+
+    public Authentication getAuthentication(){
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+
+}
